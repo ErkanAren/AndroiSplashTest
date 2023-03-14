3bd07a1fe2b79438e67056689a537d322766b537
ErkanAren <erkan.hmlz@gmail.com>
2023-03-14 14:46:33 +0200

    - New AndroidX Splash screen implemented

--- app/src/main/java/com/rknrnmmt/androisplashtest/MainActivity.kt
+++ app/src/main/java/com/rknrnmmt/androisplashtest/MainActivity.kt
@@ -2,11 +2,13 @@ package com.rknrnmmt.androisplashtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.rknrnmmt.androisplashtest.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
